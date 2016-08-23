4daf447e348141f3c15dddcff2e3d4c48e644bf8
maniacx <macs18max@gmail.com>
2016-08-24 00:06:46 +0800

    remove dex2oat no-watchdog flag and disable dex2oat using swap

--- device_leo.mk
+++ device_leo.mk
@@ -157,7 +157,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 #Art
 PRODUCT_PROPERTY_OVERRIDES += \
 	dalvik.vm.dex2oat-filter=balanced \
-	dalvik.vm.dex2oat-flags=--no-watch-dog \
+	dalvik.vm.dex2oat-swap=false \
 	dalvik.vm.image-dex2oat-filter=speed
 
 # Properties
