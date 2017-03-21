30cbe66c2d74d596ad5e6e48bbb75712731683ad
Roger Ye <shugaoye@yahoo.com>
2017-03-21 15:06:37 +0800

    Compiled recovery.

--- cm.mk
+++ cm.mk
@@ -6,7 +6,7 @@ TARGET_SCREEN_HEIGHT := 800
 TARGET_SCREEN_WIDTH := 480
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
@@ -16,7 +16,7 @@ $(call inherit-product, device/htc/htcleo/device_htcleo.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := htcleo
-PRODUCT_NAME := cm_htcleo
+PRODUCT_NAME := htcleo
 PRODUCT_BRAND := htc
 PRODUCT_MODEL := Htc Hd2
 PRODUCT_MANUFACTURER := HTC
